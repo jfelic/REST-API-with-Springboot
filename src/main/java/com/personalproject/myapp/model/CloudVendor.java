e31983e89bea19ab21979f07f77e26a79ee50a68
Julian Feliciano <felicianojulian1@gmail.com>
2023-12-12 17:25:15 -0500

    Initial commit

---
+++ src/main/java/com/personalproject/myapp/model/CloudVendor.java
@@ -0,0 +1,10 @@
+package com.personalproject.myapp.model;
+
+public class CloudVendor {
+    private String vendorID;
+    private String vendorName;
+    private String vendorAddress;
+    private String vendorPhoneNumber;
+
+    //public CloudVendor(String vendorID, String vendorName, String vendor)
+}
