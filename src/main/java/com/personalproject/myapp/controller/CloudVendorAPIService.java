2cf0097b8233f27d8c7957f39c2de590737b278f
Julian Feliciano <felicianojulian1@gmail.com>
2023-12-12 19:37:32 -0500

    Created the get, post, put, and delete mapping methods and tested them on Postman. Everything looks good.

--- src/main/java/com/personalproject/myapp/controller/CloudVendorAPIService.java
+++ src/main/java/com/personalproject/myapp/controller/CloudVendorAPIService.java
@@ -1,6 +1,10 @@
 package com.personalproject.myapp.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +13,34 @@ import com.personalproject.myapp.model.CloudVendor;
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
     
     @GetMapping("{vendorID}")
     public CloudVendor getCloudVendorDetails(String vendorID){
-        return new CloudVendor("C1", "Vendor 1", 
+        return cloudVendor;
+        
+        /* 
+        new CloudVendor("C1", "Vendor 1", 
         "Address One", "xxxxx");
+        */
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully.";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully.";
+    }
+
+    @DeleteMapping("{vendorID}")
+    public String deleteCloudVendorDetails(String vendorID) {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully.";
     }
 }
